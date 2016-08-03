1be5766fdded0d51a06ac8fdd1a78ab9459d9a8e
Lucas Christian <lchristian@twilio.com>
2016-08-03 12:17:20 -0700

    Wrap string match along word boundaries

--- match.py
+++ match.py
@@ -13,7 +13,17 @@ for match in matches:
     near_match = find_near_matches(match[0], given, max_l_dist=10)
     if near_match:
         found = near_match[0]
-        results.append(dict(search=match[0], start=found.start, end=found.end, found=given[found.start:found.end]))
+        found_str = given[found.start:found.end]
+        # Wrap beginning index into full word
+        startPos = found.start
+        while startPos == 1 or ((startPos - 1) > 0 and given[startPos - 1] != " "):
+            startPos -= 1
+        # Wrap end index into full word
+        endPos = found.end
+        while endPos == len(given) - 1 or (endPos < len(given) and given[endPos] != " "):
+            endPos += 1
+
+        results.append(dict(search=match[0], start=startPos, end=endPos, found=given[startPos:endPos]))
 
 print(json.dumps(results))
 
